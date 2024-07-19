54c7db573ab6779f5cb5902ca951278e3990d827
Amir Berenji <andy@logixsoftware.com>
2024-07-19 12:05:37 -0700

    start changing first page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,33 @@
-import Image from 'next/image'
 import Banner from '../component/banner/Banner'
 import FirstPageCard from '../component/cards/FirstPageCards'
 import FirsPageMiddleBox from '../component/middlebox/FirsPageMiddleBox'
-
+import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <Banner />
-    <section>
+    
+    {/* <Banner /> */}
+
+    <div className="w-screen h-screen grid  bg-zinc-100 ">
+      <div className='grid  grid-cols-2 '>
+        <div></div>
+        <div>
+          <Image src="/assets/circelPink.png" width="560" height="560" alt="hero 1"/>
+        </div>
+      </div>
+      
+      <div className='grid  grid-cols-2 '>
+        <div></div>
+        <div>
+          <Image src="/assets/circulePurpel.png" width="500" height="500" alt="hero 1" className="absolute bottom-0 right-32 hidden lg:block" />
+        </div>
+      </div>
+      
+        
+    </div>
+
+
+    {/* <section>
       <div className='my-5 px-5 mx-auto mb-16' >
 
         <div className='grid grid-cols-1 lg:grid-cols-5 gap-5 md:grid-cols-5 items-center object-center' >
@@ -24,10 +44,10 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </section>
+    </section> */}
 
 
-    <FirsPageMiddleBox color='bg-[#B1A296]'
+    {/* <FirsPageMiddleBox color='bg-[#B1A296]'
       desc='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
       imageSrc='https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       link='/'
@@ -56,7 +76,7 @@ export default function Home() {
       desc='Ready to take your pet ownership journey to the next level? Join our vibrant community of pet lovers from around the world. Share stories, seek advice, and connect with others who share your passion for creating a nurturing and loving environment for your pets.'
       imageSrc='assets/02.jpg'
       link='#'
-      title='Join Our Community' />
+      title='Join Our Community' /> */}
 
   </>
   )
--- app/component/banner/Banner.tsx
+++ app/component/banner/Banner.tsx
@@ -3,11 +3,19 @@ import Image from 'next/image';
 export default function Banner()
 {
     return(
-        <div className='max-sm:hidden' >
-            <div className='relative w-full h-[700px]'>
+        // <div className='max-sm:hidden' >
+        //     <div className='relative w-full h-[700px]'>
 
-                <Image src="/assets/categoryimages/banner/banner.png" fill alt='cover' objectFit='contain' />
-            </div>
+        //         <Image src="/assets/categoryimages/banner/banner.png" fill alt='cover' objectFit='contain' />
+        //     </div>
+        // </div>
+
+    <div className="w-screen h-screen flex items-center justify-center bg-zinc-200">
+        <div className="text-white text-center">
+            <h1 className="text-4xl font-bold">Full Screen Component</h1>
+            <p className="mt-4">This component covers the entire viewport.</p>
         </div>
+    </div>
+
     );    
 } 
