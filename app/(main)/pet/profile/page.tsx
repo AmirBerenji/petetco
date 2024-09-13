718089817ea34d639a8056a841fd37d6c556c309
Amir Berenji <a.berenji62@gmail.com>
2024-09-13 19:24:38 +0330

    create first part of pet profile

---
+++ app/(main)/pet/profile/page.tsx
@@ -0,0 +1,11 @@
+import MainPic from "@/app/component/general/MainPic";
+
+export default function page() {
+  return (
+    <>
+        <div className="container" >
+            <MainPic className="opacity-20" />
+        </div>
+    </>
+  )
+}
