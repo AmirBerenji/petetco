bee35f2bac08317038475d8d5ffb5f112c65f2f6
Amir Berenji <a.berenji62@gmail.com>
2025-02-05 10:42:40 +0400

    Change the petlist

--- app/(main)/pet/petlist/petListCard.tsx
+++ app/(main)/pet/petlist/petListCard.tsx
@@ -3,6 +3,7 @@ import { getAllPet } from "@/app/action/apiAction";
 import { Pet } from "@/app/models/pet";
 import { useEffect, useState } from "react";
 import PetCard from "./petCard";
+import { Button } from "flowbite-react";
 
 
 export default function PetListCardPage() {
@@ -26,6 +27,12 @@ export default function PetListCardPage() {
 
   return (
     <>
+        <div className="w-full pl-16 pt-5  " >
+          <Button className="bg-violet-600" >
+            Add New Pet
+          </Button>
+        </div>
+
           {listPet.length > 2 ? (
             <div className='container mt-20 mb-10 w-3/4 m-auto' >
               <div className='grid grid-cols-1  ' >
@@ -43,8 +50,8 @@ export default function PetListCardPage() {
             </div >
           ) : (
             <div className='container  m-auto' >
-              <div className='grid xl:grid-cols-2 gap-9 md:grid-cols-1 sm:gap-0 sm:grid-cols-1 md:gap-0' >
-                <div className='justify-items-center -ml-9 grid xl:grid-cols-1 md:grid-cols-1 sm:gap-0 sm:grid-cols-1 md:gap-0 mb-9' >
+              <div className='grid xl:grid-cols-2 xl:p-10 gap-9 md:grid-cols-1 sm:gap-0 sm:grid-cols-1 md:gap-0' >
+                <div className='justify-items-center -ml-9 grid xl:grid-cols-2 md:grid-cols-1 sm:gap-0 sm:grid-cols-1 md:gap-0 mb-9' >
                   {listPet.map((pet,i) => (
                     <>
                     <div className="w-full mt-20 " key={i} >
@@ -56,6 +63,7 @@ export default function PetListCardPage() {
               </div>
             </div >
           )}
+    
         </>
   )
 }
