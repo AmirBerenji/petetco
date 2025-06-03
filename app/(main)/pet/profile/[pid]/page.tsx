d1f24ddd3250940c383ade9f7515c03bce076f14
Amir Berenji <a.berenji62@gmail.com>
2025-06-03 20:56:11 +0400

    Change Profile

--- app/(main)/pet/profile/[pid]/page.tsx
+++ app/(main)/pet/profile/[pid]/page.tsx
@@ -22,10 +22,21 @@ export default function PetProfilePage({ params }: { params: { pid: string } }) 
         <section className="w-full border-t-8 border-t-[#F2968F] py-10">
         <div className='container grid grid-cols-1 lg:grid-cols-3 mx-auto px-10 lg:px-40 items-center'>
           <div className="col-span-1 relative">
-            <Image src={pet?.cover||''} width="300" height="300" alt="hero 1" className='' />
-            
+          
+              <div className="w-full p-3 col-span-1 flex justify-center">
+                  <div className="shadow-lg rounded-full lg:rounded-xl md:rounded-xl  ml-10   h-72 w-72 bg-slate-100 m-auto border-10 border-blue-gray-800 flex items-center justify-center overflow-hidden">
+                    {pet?.cover ? (
+                      <img
+                        src={pet.cover}
+                        className="h-full w-full object-cover"
+                      />
+                    ) : (
+                      <span className="text-gray-500">Image is not available!</span>
+                    )}
+                  </div>  
+              </div>
           </div>
-          <div className="col-span-1 lg:col-span-2 items-center flex-1 lg:px-28">
+          <div className="col-span-1 lg:col-span-2 items-center flex-1">
             <h2 className='font-bold text-xl uppercase'>{pet?.name}</h2>
             <p className='text-justify text-lg mt-5' >
               {pet?.breed.name}/{pet?.breed.kind.name}
